3f43890db35ca2b6471ab90f0c3a260d3b825a2c
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2018-12-19 19:05:30 +0100

    Implement Healthcheck Pinger
    Pings each of the healtchecks and stores the response in the database

--- api/app/services/healthcheck_pinger.rb
+++ api/app/services/healthcheck_pinger.rb
@@ -1,9 +1,44 @@
 class HealthcheckPinger
+  def initialize(endpoint)
+    @endpoint = endpoint
+  end
+
   def self.ping(endpoint)
-    true
+    new(endpoint).ping
   end
 
   def self.ping_all(healthcheck)
     healthcheck.endpoints.each { |ep| ping(ep) }
   end
+
+  def ping
+    send_request = endpoint.pings.create!
+    request_data(send_request)
+  end
+
+  private
+
+  attr_reader :endpoint
+
+  def request_data(result_ping)
+    response = Faraday.get(endpoint.remote_url)
+    if response.status == 200
+      handle_response(result_ping, response.body, success: true)
+    else
+      handle_response(result_ping, response.body)
+    end
+    response.body
+  rescue Faraday::Error => e
+    handle_response(result_ping, e.message)
+  end
+
+  def handle_response(result_ping, response, success: false)
+    result_ping.response = response
+    if success
+      result_ping.status_success!
+    else
+      result_ping.status_error!
+    end
+  end
+
 end
--- api/spec/services/healthcheck_pinger_spec.rb
+++ api/spec/services/healthcheck_pinger_spec.rb
@@ -1,6 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe HealthcheckPinger do
+  describe ".ping" do
+    it "sends a request to the given url" do
+      allow(Faraday).to receive(:get) { OpenStruct.new(body: "yes", status: 200) }
+
+      described_class.ping(create(:endpoint))
+
+      expect(Faraday).to have_received(:get).exactly(1).times
+    end
+
+    it "stores the response in the database" do
+      endpoint = create(:endpoint)
+      allow(Faraday).to receive(:get) { OpenStruct.new(body: "foobar", status: 200) }
+
+      described_class.ping(endpoint)
+      ping = endpoint.pings.first
+      expect(ping).to be_present
+      expect(ping.status_success?).to be_truthy
+      expect(ping.response).to eq("foobar")
+    end
+
+    it "stores the response in the database when there was an error" do
+      endpoint = create(:endpoint)
+      allow(Faraday).to receive(:get) { raise Faraday::Error.new("error") }
+
+      described_class.ping(endpoint)
+      ping = endpoint.pings.first
+      expect(ping).to be_present
+      expect(ping.status_error?).to be_truthy
+      expect(ping.response).to eq("error")
+    end
+
+    it "stores the response in the database when there was an error http status code" do
+      endpoint = create(:endpoint)
+      allow(Faraday).to receive(:get) { OpenStruct.new(body: "wrong status code", status: 401) }
+
+      described_class.ping(endpoint)
+      ping = endpoint.pings.first
+      expect(ping).to be_present
+      expect(ping.status_error?).to be_truthy
+      expect(ping.response).to eq("wrong status code")
+    end
+  end
+
+
   describe ".ping_all" do
     it "pings every health service" do
       hc = create(:healthcheck)
