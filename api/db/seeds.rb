cd2b4df956147bc52b320a26519066feffe779a2
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2018-12-20 21:02:33 +0100

    Scaffold Active Admin

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -9,3 +9,10 @@ seed_data("access key") do
   FactoryBot.create(:access_key)
 end
 
+seed_data("active admin user") do
+  AdminUser.create!(
+    email: 'admin@example.com',
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
